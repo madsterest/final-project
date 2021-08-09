f3b508e582334d19f5844e4f3be55fdae5994407
madsterest <odea.madeleine@gmail.com>
2021-08-09 21:02:33 +1000

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+console.log(process.env.MONGODB_URI);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/recipe-site", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/recipe-site", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const path = require("path");
 const db = require("./config/connection");
 const fileupload = require("express-fileupload");
