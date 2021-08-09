df1d21a8899c51c2b1a315247cb01b933f02cbfb
madsterest <odea.madeleine@gmail.com>
2021-08-09 18:02:23 +1000

    fixed server

--- server/server.js
+++ server/server.js
@@ -17,14 +17,12 @@ app.use(express.static(path.join(__dirname, "uploads")));
 app.use(routes);
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
